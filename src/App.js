7e0c35e17c376c4602371d2017764a6edd6141ea
Claire Muscat <clairemuscat94@gmail.com>
2020-07-15 13:51:40 -0500

    background image works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+let imageUrl = "./src/background-image-plants.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className="App-content">
+        <h1>
+          Hi! I'm Claire. Software developer, crossword constructor and cocktail
+          enthusiast. It's nice to meet you!
+        </h1>
+      </div>
     </div>
   );
 }
